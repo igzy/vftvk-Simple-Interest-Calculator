77ad3d9e2f8278d47e2579939803b1a57e442a35
iggy <marianne.ignacio@gmail.com>
2022-02-23 00:17:49 +0800

    Calculator

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function outputUpdate(rate) {
+	document.querySelector('#rate').value = rate;
 }
+
+function compute(){
+    //create references to the input elements we wish to validate
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("select_years").value;
+    let date =  new Date().getFullYear();
+    //check for amount
+    if(principal == ""){
+        alert("Please enter the principal amount");
+	    principal.focus();
+    }
+
+    //check for rate
+    if(rate == 0){
+        alert("Rate cannot be 0");
+	    rate.focus();
+    }
+
+    var interest = (principal * rate * years) / 100;
+    // alert(parseFloat(date)+parseFloat(years));
+    var mySpan = document.getElementById("interest");
+    
+    document.getElementById("amount_val").textContent = principal;
+    document.getElementById("rate_val").textContent = rate;
+    document.getElementById("interest_val").textContent = interest;
+    document.getElementById("year_val").textContent = parseFloat(date)+parseFloat(years);
+    mySpan.style.display = "";
+
+}
+
+
+
         
